148dc3f7e6d4da751fd3e00e69288d848b741924
DonkeyFuck64 <44863235+DonkeyFuck64@users.noreply.github.com>
2018-11-08 11:20:44 +0100

    Add files via upload

---
+++ bfk_Projekt/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bfk_Projekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            args[0] = "list";
+            if(args.Length < 1)
+            {
+                 return;
+            }
+            else if (args[0].Equals("list"))
+            {
+                try
+                {
+                    string arg = args[1];
+                    list.listAll(arg);
+
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    Console.WriteLine("Parameter ist Invalide.");
+                }
+            }
+            else if (args[0].Equals("listtype"))
+            {
+                if (args[1].Equals(""))
+                {
+
+                }
+            }
+            else if (args[0].Equals("listname"))
+            {
+                if (args[1].Equals(""))
+                {
+
+                }
+            }
+            else if (args[0].Equals( "backup"))
+            {
+                if (args[1].Equals(""))
+                {
+
+                }
+            }
+            else if (args[0].Equals("xml"))
+            {
+                
+            }
+            else if (args[0].Equals("join"))
+            {
+                string[] files = args;
+            }
+            else if (args[0].Equals("attrib"))
+            {
+                string[] files = args;
+            }
+            else if (args[0].Equals("rename"))
+            {
+                string[] files = args;
+            }
+            else if (args[0] .Equals("find txt"))
+            {
+                string[] files = args;
+            }
+            else if (args[0].Equals("censorship"))
+            {
+                string[] files = args;
+            }
+
+
+            
+
+        }
+    }
+}
---
+++ bfk_Projekt/list.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace bfk_Projekt
+{
+    class list
+    {
+        public static void listAll(string arg)
+        {
+
+
+            var files = from retrievedFile in Directory.EnumerateFiles("dir//", "*.txt", SearchOption.AllDirectories)
+                        from line in File.ReadLines(retrievedFile)
+                        where line.Contains("Example")
+                        select new
+                        {
+                            File = retrievedFile,
+                            Line = line
+                        };
+
+            foreach (var f in files)
+            {
+                Console.WriteLine("{0} contains {1}", f.File, f.Line);
+            }
+            Console.WriteLine("{0} lines found.", files.Count().ToString());
+        }
+    }
+}
+
