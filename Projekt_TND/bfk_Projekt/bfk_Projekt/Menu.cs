2f72097f1ffbcf52d5bed4dc7169cc411299125f
DonkeyFuck64 <44863235+DonkeyFuck64@users.noreply.github.com>
2018-11-08 10:04:51 +0100

    Add files via upload

---
+++ Projekt_TND/bfk_Projekt/bfk_Projekt/Menu.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bfk_Projekt
+{
+    static class Menu
+    {
+
+        public static void Menue()
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader("dir\\Testfile.txt"))
+                {
+                    String line = sr.ReadToEnd();
+                    Console.WriteLine(line);
+                    Console.ReadKey();
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+                Console.ReadKey();
+            }
+        }
+    }
+}
+
---
+++ Projekt_TND/bfk_Projekt/bfk_Projekt/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bfk_Projekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args[0] == "list")
+            {
+            }
+            else if (args[0] == "listtype")
+            {
+                if (args[1] == "")
+                {
+
+                }
+            }
+            else if (args[0] == "xml" && args[1] != "")
+            {
+                
+            }
+            else if (args[0] == "join")
+            {
+                string files = args;
+            }
+            
+
+        }
+    }
+}
